cdb4d6f9beb649357e3e3c8c3973e7e32ba0c268
vietph <vietph@hehe.com>
2023-10-23 11:48:29 +0700

    Test

--- train.py
+++ train.py
@@ -89,7 +89,9 @@ def train(
         with fabric.no_backward_sync(model, enabled=is_accumulating):
             # print(model.device)
             # print(input_ids.device)
-            loss = model(input_ids)["loss"]
+            loss = model(input_ids)
+            print("loss type", type(loss))
+            print("loss[0]", loss[0])
             # import pdb
             # pdb.set_trace()
             # print(logits)
